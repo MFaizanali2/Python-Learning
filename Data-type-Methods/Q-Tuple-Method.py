ccf30fa671a22ab31ec0db00f83d0d713bd6f2ce
M Faizan Ali <faizanabid532@gmail.com>
2025-11-14 15:41:01 +0500

    Chapter-07-Q

---
+++ Data-type-Methods/Q-Tuple-Method.py
@@ -0,0 +1,60 @@
+        # Python Tuple Practice Questions
+
+        # Question NO 01
+# Ek tuple t = (1, 2, 3, 2, 4) me "2" kitni baar aaya hai count karne ka method likho.
+# t: tuple = (1, 2, 3, 2, 4)
+# print(t.count(2))
+
+
+        # Question NO 02
+# Tuple t = ('a', 'b', 'c', 'd') me "c" ka index kaise find karoge?
+# t: tuple = ('a', 'b', 'c', 'd')
+# print(t.index('c'))
+
+
+        # Question NO 03
+# Ek tuple me last element kaise access karte ho?
+# t: tuple = ('a', 'b', 'c', 'd')
+# print(t.index('d'))
+
+
+        # Question NO 04
+# Ek tuple me first 3 elements ka slice kaise nikaloge?
+# t: tuple = ('a', 'b', 'c', 'd')
+# print(t[0:3])
+
+
+        # Question NO 05
+# Tuple t = (1, 2, 3) me ek new element 4 add karne ka direct method hai ya nahi? Agar nahi, to kya karenge?
+# Answer : Change nahi hota hai pehla isa list ma chnage karna hoga phir ya change hoga
+
+
+
+        # Question NO 06
+# Tuple t = (5, 10, 15) me maximum value kaise find karoge?
+# t: tuple = (1, 2, 3, 2, 4)
+# print(max(t))
+
+
+        # Question NO 07
+# Tuple t = (7, 8, 9) me minimum value kaise find karoge?
+# t: tuple = (1, 2, 3, 2, 4)
+# print(min(t))
+
+
+        # Question NO 08
+# Tuple t = (1, 2, 3, 4, 5) ko reverse order me print kaise karoge?
+# t: tuple = (1, 2, 3, 2, 4)
+# print(t[::-1])
+
+
+        # Question NO 09
+# Tuple me kisi value ka presence check karne ke liye kaunsa operator use hota hai?
+# t: tuple = (1, 2, 3, 2, 4)
+# print(3 in t)
+
+
+        # Question NO 10
+# Tuple t = (10, 20, 30) ke elements ko alag variables me assign karne ke liye kya technique use hoti hai?
+# t: tuple = (10, 20, 30)
+# a, b, c = t
