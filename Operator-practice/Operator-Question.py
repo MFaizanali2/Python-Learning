adb5c9a1fc62e1e5063919aa325215e26a301383
M Faizan Ali <faizanabid532@gmail.com>
2025-11-19 17:01:56 +0500

    Operator Question Solve

---
+++ Operator-practice/Operator-Question.py
@@ -0,0 +1,114 @@
+                #  20 Python Operator Practice Questions
+
+# 1. Arithmetic operator use karke 2 numbers ka sum, product aur remainder kaise find karoge?
+# a: int = 5
+# b: int = 5
+# print(a+b)
+
+
+# 2. Agar a = 20 aur b = 3 ho, to floor division (//) ka output kya hoga?
+# a: int = 20
+# b: int = 3
+# print(a//b)
+
+
+# 3. a = 2 aur b = 5 ho to exponent operator (**) ka result kya milega?
+# a: int = 20
+# b: int = 3
+# print(a**b)
+
+
+# 4. Assignment operator += use karke variable x = 10 ko 15 kaise banaoge?
+# a: int = 10
+# a += 5
+# print(a)
+
+
+# 5. Agar x = 25 ho to /= operator use karne ke baad x /= 5 ka result kya hoga?
+# x: int = 25
+# x /= 5
+# print(x)
+
+
+
+# 6. Comparison operator use karke check karo:
+# a: int = 10
+# b: int = 20
+# print(a > b)
+
+
+# a = 10, b = 10 → kya a == b True hoga?
+# a: int = 10
+# b: int = 10
+# print(a == b)
+
+
+# 7. Comparison operator use karke check karo:
+# a: int = 10
+# b: int = 10
+# print(a <= b)
+
+
+# 5 > 10 ka result kya hoga?
+# print(5 > 10)
+
+# 8. Logical operator and use karke check karo:
+# print((10 > 5) or (5 > 3))
+
+
+# (10 > 5) and (5 > 3) ka result kya hoga?
+# print((10 > 5) and (5 > 3))
+
+
+# 9. Logical operator or use karke check karo:
+# print((10 > 5) or (5 > 3))
+
+
+# (3 > 5) or (4 == 4) ka output kya hoga?
+# print((3 > 5) or (4 == 4))
+
+
+# 10. Logical not operator use karke: not (10 == 10) ka result kya hoga?
+# print(not (10 == 10))
+
+
+# 11. Membership operator in use karke check karo: 2 in [1, 2, 3] → True ya False?
+# print(2 in [1, 2, 3])
+
+# 12. Membership operator not in use karke check karo: "a" not in "hello" → True ya False?
+
+a: str = "hello"
+print(a in "hello")
+
+# 13. Identity operator use karke check karo:        
+
+# a = [1, 2]
+00000000000000000# kya a is b True hoga?
+
+# 14. Identity operator use karke check karo:
+
+# a = [1, 2]
+# b = [1, 2]
+# kya a is b False hoga? Kyun?
+
+# 15. Bitwise AND operator (&) ka result kya hoga:
+
+# 5 & 3 ?
+
+# 16. Bitwise OR operator (|) ka result kya hoga:
+
+# 5 | 3 ?
+
+# 17. Bitwise XOR operator (^) ka result kya hoga:
+
+# 7 ^ 2 ?
+
+# 18. Left shift operator (<<) use karke:
+
+# 4 << 1 ka answer kya hoga?
+
+# 19. Right shift operator (>>) use karke:
+
+# 8 >> 2 ka answer kya hoga?
+
+# 20. Ek list nums = [10, 20, 30] me check karo ke 40 present hai ya nahi (in operator use karo).
---
+++ Operator-practice/Opertor-Practice.py
@@ -0,0 +1,58 @@
+        # Arthamatic Operator
+
+# a: int  = int(input("Enter Your Number 1: "))
+# b: int  = int(input("Enter Your Number 2: "))
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a**b)
+# print(a//b)
+
+
+        # Assigment Operator
+
+# x = 5
+# x += 3
+
+# x = 5
+# x -= 3
+
+# x = 5
+# x %= 3
+
+# x = 5
+# x **= 3
+# print(x) 
+
+
+        # Assigment Operator
+
+# a = 10
+# b = 15
+
+# print(a == b)
+# print(a != b)
+# print(a >= b)
+# print(a <= b)
+# print(a > b)
+# print(a < b)
+
+
+        # Assigment Operator
+
+# x = 5
+
+# print(x > 3 and x < 10)
+# print(x > 10 or x == 5)
+# print(not(x == 5))
+
+
+        # Identity Operator
+
+# nums = [1, 2, 3]
+
+# print(2 in nums)
+# print(5 not in nums)
