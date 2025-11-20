71a38cb03554658d95a9f74508094c9f971728ac
M Faizan Ali <faizanabid532@gmail.com>
2025-11-20 17:43:11 +0500

    Function Practice

--- Chapter-07-PS/PracticeSet7-2.py
+++ Chapter-07-PS/PracticeSet7-2.py
@@ -1,3 +1,38 @@
-name = "Hello Bhai"
+# name = "Hello Bhai"
 
-print(name.upper())
+# print(name.upper())
+
+# n = int(input("Enter Your Number: "))
+
+# for i in range(1, n+1):
+#     print(" "* (n-i), end="")
+#     print("*"* (2*i-1), end="")
+#     print("")
+
+
+
+# n = int(input("Enter Your Number: "))
+
+# for i in range(1, n+1):
+#     print("*"* i, end="")
+#     print("")
+
+
+
+# n = int(input("Enter Your Number: "))
+
+# for i in range(1, n+1):
+#     if(i==1 or i==n):
+#         print("*"* n, end="")
+#     else:
+#         print("*", end="")
+#         print(" "* (n-2), end="")
+#         print("*", end="")
+#     print("")
+
+
+# n = int(input("Enter Your Number: "))
+
+# for i in range(1, 11):
+    # print(f"{n} X {i} = {n*i}")
+    # print(f"{n} X {11-i} = {n*(11-i)}")
